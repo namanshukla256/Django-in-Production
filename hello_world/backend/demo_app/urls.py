1b49ab53b41f3618831e548722ae5efd3518d039
Naman Shukla <namanshukla256@gmail.com>
2025-05-26 23:57:38 +0530

    gg

--- hello_world/backend/demo_app/urls.py
+++ hello_world/backend/demo_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('hello-world/', views.hello_world),
     path('hello-world-drf/', views.hello_world_drf),
     path('demo-version/', views.demo_version),
+    path('custom-version/', views.DemoView.as_view()),
+    path('another-custom-version/', views.AnotherView.as_view())
 ]
